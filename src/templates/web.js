0a4855471e5587f0ccb968dc6db92380d283ddfd
otofune <otofune@gmail.com>
2017-04-07 10:13:07 +0900

    :herb: [templates/web] according to the convention

--- src/templates/web.js
+++ src/templates/web.js
@@ -26,7 +26,7 @@ module.exports = (values, apiKey) => {
         apiServerIp: "127.0.0.1",
         apiServerPort: values.ports.api,
         cookiePass: random(),
-        sessionKey: random(),
+        sessionKey: "hmsk",
         sessionSecret: random(),
         googleRecaptchaSecret: values.recaptcha.secret,
         publicConfig: {
