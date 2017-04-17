bf129d1c847e4be361db34a53aa9da7adee17082
otofune <otofune@gmail.com>
2017-04-17 22:02:09 +0900

    :muscle: [index] add debug feature
    and add some comments.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ var apiTemplate = require("./templates/api")
 var fileTemplate = require("./templates/file")
 /** parts */
 var asker = require("./asker")
+/** check process in debug mode */
+var debug = -1 !== process.argv.indexOf("--debug")
 
 /**
  * shit main
@@ -19,9 +21,23 @@ var main = async () => {
     var baseDir = path.join(current, "..")
     var storeDir = path.join(baseDir, "store")
 
-    var values = await asker()
+    /** get nesessary values for generation */
+    var values
+    // if debug mode, try to read values from values.json
+    if (debug) try {
+        values = JSON.parse(fs.readFileSync(path.join(storeDir, "values.json")))
+    } catch (e) {
+        console.log(e)
+    }
+    if (! values) values = await asker()
+    // if debug mode, output values object to console
+    if (debug) console.log(JSON.stringify(values, null, 4))
+
+    /** generate random keys */
     var apiKey = random()
     var fileKey = random()
+
+    /** make values to various setting objects */
     var web = webTemplate(values, apiKey)
     var api = apiTemplate(values, apiKey, fileKey)
     var file = fileTemplate(values, fileKey)
@@ -38,6 +54,8 @@ var main = async () => {
         fs.writeFileSync(path.join(storeDir, "web.json"), JSON.stringify(web, null, 4))
         fs.writeFileSync(path.join(storeDir, "api.json"), JSON.stringify(api, null, 4))
         fs.writeFileSync(path.join(storeDir, "file.json"), JSON.stringify(file, null, 4))
+        // if debug mode, save values object to values.json
+        if (debug) fs.writeFileSync(path.join(storeDir, "values.json"), JSON.stringify(values, null, 4))
     } catch (e) {
         console.dir(e)
     }
